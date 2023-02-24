5554da4966dd87584352b0d9f689444d37eafac0
Emil Tang Kristensen <etkr@universal-robots.com>
2023-02-24 13:34:34 +0100

    c0rs

--- apps/snake/module-federation.config.js
+++ apps/snake/module-federation.config.js
@@ -3,4 +3,8 @@ module.exports = {
   exposes: {
     './Module': 'apps/snake/src/app/remote-entry/entry.module.ts',
   },
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+  }
 };
